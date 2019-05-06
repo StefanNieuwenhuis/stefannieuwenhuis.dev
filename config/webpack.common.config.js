1ca1a7973db5040bc8e0ac7c6d7ab8652c9879c5
Stefan Nieuwenhuis <snieuwenhuis@bol.com>
2019-05-06 13:31:08 +0200

    update gitignore

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../public')
     },
     resolve: {
         extensions: ['.js', '.ts']

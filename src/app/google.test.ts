b9b071eb84bf6d36c66581ebb09971770cb6e8d7
Stefan Nieuwenhuis <snieuwenhuis@bol.com>
2019-06-14 14:31:47 +0200

    feat(testing): add puppeteer & jest

---
+++ src/app/google.test.ts
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+let browser: any;
+let page: any;
+
+describe('Google', () => {
+    beforeEach(async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+    });
+
+    afterEach(async () => {
+        await browser.close();
+    });
+
+    it('should open a new page', async () => {
+        await page.goto('https://google.com');
+        await browser.close();
+    })
+
+    it('should display "google" text on page', async () => {
+        await expect(page).toMatch('google');
+      })
+});

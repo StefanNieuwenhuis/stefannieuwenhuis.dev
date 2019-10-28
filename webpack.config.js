16e5cec01fd56da8607236ad42c676f3d3b4e83d
Stefan Nieuwenhuis <stefannhs@gmail.com>
2019-10-28 18:43:14 +0100

    add Angular to the game

--- webpack.config.js
+++
@@ -1,3 +0,0 @@
-const environment = (process.env.NODE_ENV || 'development').trim();
-
-module.exports = environment === 'development' ? require('./config/webpack.dev.config.js') : require('./config/webpack.prod.config.js');

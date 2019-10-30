b06a9ed785936a2fd7b1fae7e35f8ce60ef29efa
Stefan Nieuwenhuis <stefannhs@gmail.com>
2019-10-30 11:42:49 +0100

    update unit tests

--- src/app/blog/containers/posts/posts.component.spec.ts
+++ src/app/blog/containers/posts/posts.component.spec.ts
@@ -1,24 +1,34 @@
+
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import { Store } from '@ngrx/store';
+
+import { cold } from 'jasmine-marbles';
 
 import { PostsComponent } from './posts.component';
 import { RootStoreState } from '../../../root-store';
 import { Post } from '../../../models';
-import { PostsActions } from '../../../root-store/posts';
+
 
 /**
  * Stubbed data
  */
-const postsStateMock = {
+const posts: Post[] = [
+  { id: '1', categoryId: null, title: 'A fake title #1', body: 'A fake body #1', publishDate: '22-05-1987' },
+  { id: '2', categoryId: null, title: 'A fake title #2', body: 'A fake body #2', publishDate: '22-05-1987' },
+];
+
+const postsStateMock: RootStoreState.AppState = {
   posts: {
     entities: {
       '1': { id: '1', categoryId: null, title: 'A fake title #1', body: 'A fake body #1', publishDate: '22-05-1987' },
       '2': { id: '2', categoryId: null, title: 'A fake title #2', body: 'A fake body #2', publishDate: '22-05-1987' },
     },
-    ids: ['1', '2']
-  }
+    ids: ['1', '2'],
+    selectedPostId: null,
+    error: null,
+    loading: false
+  },
 };
 
 /**
@@ -32,7 +42,6 @@ class PostsOverviewStubComponent {
 fdescribe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
-  let store: MockStore<RootStoreState.AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -47,9 +56,6 @@ fdescribe('PostsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PostsComponent);
     component = fixture.componentInstance;
-    store = TestBed.get(Store);
-    spyOn(store, 'dispatch').and.callThrough();
-
     fixture.detectChanges();
   });
 
@@ -58,9 +64,8 @@ fdescribe('PostsComponent', () => {
   });
 
   it('should dispatch an LoadAllPosts Action in ngOnInit lifecycle', () => {
-    const action = PostsActions.loadAllPosts();
+    const expected = cold('a', { a: posts });
 
-    expect(store.dispatch).toHaveBeenCalledWith(action);
-    component.posts$.subscribe((posts: Post[]) => expect(posts.length).toBe(2));
+    expect(component.posts$).toBeObservable(expected);
   });
 });

72e29360e1b49447679376586b0c4d289b760c4f
Stefan Nieuwenhuis <stefannhs@gmail.com>
2019-11-06 12:02:42 +0100

    update CircleCI config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'blog' },
   { path: 'blog', loadChildren: () => import('./blog/blog.module').then(module => module.BlogModule) }
 ];
 

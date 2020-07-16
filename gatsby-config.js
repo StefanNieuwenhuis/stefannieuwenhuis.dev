f27e08d2374a7649d24172273dbb796423dbfa71
Stefan Nieuwenhuis <stefannhs@gmail.com>
2020-07-16 14:32:49 +0200

    add styling  and list posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,6 @@ module.exports = {
         name: `posts`,
       },
     },
+    `gatsby-theme-material-ui`,
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import { graphql } from 'gatsby';
+import { Layout } from '../components/Layout';
 
-export default () => {
-  const { title, description } = useSiteMetadata();
-  return (
-    <>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </>
-  );
-};
+export default ({ data }) => (
+  <>
+    <Layout>
+      {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+        <>
+          <h1>{frontmatter.title}</h1>
+          <p>{frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </>
+      ))}
+    </Layout>
+  </>
+);
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
